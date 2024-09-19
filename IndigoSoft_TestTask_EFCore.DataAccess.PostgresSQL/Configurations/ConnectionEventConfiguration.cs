e4228448fbf6c7afb8ed6f9f3c1da038968b8e43
Sergey Eremin <sergey@ensed.org>
2024-09-19 10:17:30 +0300

    added data access configurations

---
+++ IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL/Configurations/ConnectionEventConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL.Configurations;
+
+public class ConnectionEventConfiguration : IEntityTypeConfiguration<ConnectionEventEntity>
+{
+    public void Configure(EntityTypeBuilder<ConnectionEventEntity> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder.HasOne(c => c.User).WithMany(u => u.ConnectionEvents).HasForeignKey(c => c.UserId);
+        builder.HasOne(c => c.IpAddress).WithMany(i => i.ConnectionEvents).HasForeignKey(c => c.IpAddressId);
+    }
+}
---
+++ IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL/Configurations/UserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
+{
+    public void Configure(EntityTypeBuilder<UserEntity> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.HasMany(u => u.ConnectionEvents).WithOne(c => c.User);
+    }
+}
