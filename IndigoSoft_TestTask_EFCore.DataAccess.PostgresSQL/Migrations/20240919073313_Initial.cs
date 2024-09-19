921ad4db1ec2b916129ad1bd3d85c0ef2d16fbbe
Sergey Eremin <sergey@ensed.org>
2024-09-19 10:34:23 +0300

    added the initial db migrations

---
+++ IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL/Migrations/20240919073313_Initial.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IpAddresses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Value = table.Column<string>(type: "varchar", maxLength: 45, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IpAddresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountNumber = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConnectionEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DateAndTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IpAddressId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConnectionEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ConnectionEvents_IpAddresses_IpAddressId",
+                        column: x => x.IpAddressId,
+                        principalTable: "IpAddresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ConnectionEvents_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConnectionEvents_IpAddressId",
+                table: "ConnectionEvents",
+                column: "IpAddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConnectionEvents_UserId",
+                table: "ConnectionEvents",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IpAddresses_Value",
+                table: "IpAddresses",
+                column: "Value",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_AccountNumber",
+                table: "Users",
+                column: "AccountNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConnectionEvents");
+
+            migrationBuilder.DropTable(
+                name: "IpAddresses");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
