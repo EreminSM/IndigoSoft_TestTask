fee33fb7fb317c953c7f9df3067a19dc3e6054f8
Sergey Eremin <sergey@ensed.org>
2024-09-19 10:23:49 +0300

    implemented a class for db context definition

---
+++ IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL/IndigoSoftTestTaskDbContext.cs
@@ -0,0 +1,31 @@
+﻿using IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL.Configurations;
+using IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL;
+
+public class IndigoSoftTestTaskDbContext : DbContext
+{
+    public IndigoSoftTestTaskDbContext(DbContextOptions<IndigoSoftTestTaskDbContext> options)
+    : base(options)
+    {
+
+    }
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<IpAddressEntity> IpAddresses { get; set; }
+    public DbSet<ConnectionEventEntity> ConnectionEvents { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new IpAddressConfiguration());
+        modelBuilder.ApplyConfiguration(new ConnectionEventConfiguration());
+
+        modelBuilder.Entity<IpAddressEntity>().Property(i => i.Value).HasColumnType("varchar").HasMaxLength(45);
+        modelBuilder.Entity<IpAddressEntity>().HasIndex(i => i.Value).IsUnique();
+
+        modelBuilder.Entity<UserEntity>().HasIndex(u => u.AccountNumber).IsUnique();
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
