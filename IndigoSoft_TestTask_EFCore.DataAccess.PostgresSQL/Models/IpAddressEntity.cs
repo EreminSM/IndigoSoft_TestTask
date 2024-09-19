a0e57060c57d832f313ed06ac2a61929ee827494
Sergey Eremin <sergey@ensed.org>
2024-09-19 10:14:01 +0300

    added data access models

---
+++ IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL/Models/IpAddressEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IndigoSoft_TestTask_EFCore.DataAccess.PostgresSQL.Models;
+
+public class IpAddressEntity
+{
+    public Guid Id { set; get; }
+
+    [Required]
+    public string Value { set; get; } = string.Empty;
+    public List<ConnectionEventEntity> ConnectionEvents { set; get; } = new();
+}
